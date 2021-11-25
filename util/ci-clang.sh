9db8451d65cceb0a510380cdb178eb279bae18ed
mj-xmr <mjxmr@protonmail.com>
2021-11-25 07:36:55 +0100

    Clang in matrix

--- util/ci-clang.sh
+++ util/ci-clang.sh
@@ -9,10 +9,13 @@ CMAKE_C_COMPILER="${GCC_PREFIX}"
 CMAKE_CXX_COMPILER="${GCC_PREFIX}++"
 
 mkdir -p $BUILD_DIR && cd $BUILD_DIR
+mkdir -p bin
 
 cmake -G "${GEN}" ../.. \
 -DCMAKE_C_COMPILER="${CMAKE_C_COMPILER}" \
 -DCMAKE_CXX_COMPILER="${CMAKE_CXX_COMPILER}" \
--DUSE_STATIC="ON" \
+-DUSE_STATIC="OFF" \
 -DUSE_UNITY="ON" \
+-DBUILD_BOOST="ON" \
 && ${MAKE} -j `nproc` || ${MAKE} && ${MAKE} -j `nproc` install
+ctest --output-on-failure -j `nproc`
--- util/run-demo.sh
+++ util/run-demo.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -e
 
+COMPILER="gcc"
+
+if [ ! -z $1 ]; then
+	COMPILER="$1"
+fi
+
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:lib
 
-pushd build/gcc/bin
+pushd "build/$COMPILER/bin"
 	./tsqsim --help
 	./tsqsim
 popd
