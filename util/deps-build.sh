3a94b1f51c5809009d88b47056b2e196e18fc217
mj-xmr <mjxmr@protonmail.com>
2021-12-02 19:52:32 +0100

    Build QCP

--- util/deps-build.sh
+++ util/deps-build.sh
@@ -2,4 +2,8 @@
 
 echo "Building dependencies"
 # externals/URT/build/build-urt.sh # Not needed anymore. Using CMakeLists.txt
+externals/scripts/build-qcustomplot.sh
+
 # Other dependencies here...
+
+
