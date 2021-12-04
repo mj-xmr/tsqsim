e679e00fca603756f6c2f1c7fc51e73d10ae2e4c
mj-xmr <mjxmr@protonmail.com>
2021-12-04 21:17:22 +0100

    qt5-default

--- util/deps-pull.sh
+++ util/deps-pull.sh
@@ -7,7 +7,7 @@ if [ "$(uname)" == "Darwin" ]; then
 	# TODO: armadillo should be replaced with Eigen (at URT)
 	# TODO: install qt instead of qt5, after moving to latest QCustomPlot. Then the "--prefix" and "link" aren't needed.
 else
-	sudo apt update; sudo apt -y install build-essential cmake ccache gnuplot libssl-dev libeigen3-dev libunittest++-dev libgsl-dev libboost-system-dev libboost-filesystem-dev libboost-iostreams-dev libboost-serialization-dev libboost-program-options-dev libboost-date-time-dev libboost-atomic-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
+	sudo apt update; sudo apt -y install build-essential cmake ccache gnuplot libssl-dev libeigen3-dev libunittest++-dev libgsl-dev libboost-system-dev libboost-filesystem-dev libboost-iostreams-dev libboost-serialization-dev libboost-program-options-dev libboost-date-time-dev libboost-atomic-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools qt5-default
 fi
 # TODO: implement MinGW
 
