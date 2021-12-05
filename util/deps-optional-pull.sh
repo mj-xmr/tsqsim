6df2ba0ec422ece70ef852e99fb42a0ec452a8c2
mj-xmr <mjxmr@protonmail.com>
2021-12-05 22:50:44 +0100

    Add deps optional stub

--- util/deps-optional-pull.sh
+++ util/deps-optional-pull.sh
@@ -4,6 +4,10 @@
 #	HOMEBREW_NO_AUTO_UPDATE=1 brew install mkl
 #else
 #	sudo apt update; sudo apt -y install libmkl-dev # TODO: Enable once it makes sense
+
+#apt-get install libmkl-dev # Needs up to 2 GB of space.
+# Non interactive alternative (deprecated):
+#DEBIAN_FRONTEND=noninteractive apt-get install -y -q --force-yes libmkl-dev
 #fi
 
 
