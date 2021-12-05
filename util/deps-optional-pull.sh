c1daacebb776647d6100ee7ad05411c61b0c4c26
mj-xmr <mjxmr@protonmail.com>
2021-12-05 22:49:38 +0100

    Add deps optional stub

---
+++ util/deps-optional-pull.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+#if [ "$(uname)" == "Darwin" ]; then
+#	HOMEBREW_NO_AUTO_UPDATE=1 brew install mkl
+#else
+#	sudo apt update; sudo apt -y install libmkl-dev # TODO: Enable once it makes sense
+#fi
+
+
