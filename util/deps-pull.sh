276ca293e6ee7c0ee2548b026dcf51f3f86fdeb2
mj-xmr <mjxmr@protonmail.com>
2021-12-06 09:25:16 +0100

    brew link force utcpp

--- util/deps-pull.sh
+++ util/deps-pull.sh
@@ -8,9 +8,7 @@ if [ "$(uname)" == "Darwin" ]; then
 	fi
 	#HOMEBREW_NO_AUTO_UPDATE=1 brew install qt5
 	#brew link -v qt5 --force
-	#HOMEBREW_NO_AUTO_UPDATE=1 brew install boost openssl ccache gnuplot eigen gsl unittest-cpp
-	HOMEBREW_NO_AUTO_UPDATE=1 brew install -vd unittest-cpp
-	# TODO: armadillo should be replaced with Eigen (at URT)
+	HOMEBREW_NO_AUTO_UPDATE=1 brew install boost openssl ccache gnuplot eigen gsl unittest-cpp
 	# TODO: install qt instead of qt5, after moving to latest QCustomPlot. Then the "--prefix" and "link" aren't needed.
 else
 	sudo apt update; sudo apt -y install build-essential cmake ccache gnuplot libssl-dev libeigen3-dev libunittest++-dev libgsl-dev libboost-system-dev libboost-filesystem-dev libboost-iostreams-dev libboost-serialization-dev libboost-program-options-dev libboost-date-time-dev libboost-atomic-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
