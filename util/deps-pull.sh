e5abdecb5c478a52b6d2a2830b7a0506e7d19bdb
mj-xmr <mjxmr@protonmail.com>
2021-12-09 22:13:13 +0100

    Win: unifying prep scripts, part 1

--- util/deps-pull.sh
+++ util/deps-pull.sh
@@ -10,10 +10,11 @@ if [ "$(uname)" == "Darwin" ]; then
 	#brew link -v qt5 --force
 	HOMEBREW_NO_AUTO_UPDATE=1 brew install boost openssl ccache gnuplot eigen gsl unittest-cpp
 	# TODO: install qt instead of qt5, after moving to latest QCustomPlot. Then the "--prefix" and "link" aren't needed.
-elif [ "$(uname)" == "MinGw" ]; then
-	echo "TODO: install deps"	
-else
+elif [ "$(uname)" == "Linux" ]; then
 	sudo apt update; sudo apt -y install build-essential cmake ccache gnuplot libssl-dev libeigen3-dev libunittest++-dev libgsl-dev libboost-system-dev libboost-filesystem-dev libboost-iostreams-dev libboost-serialization-dev libboost-program-options-dev libboost-date-time-dev libboost-atomic-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
+else
+	echo "TODO: install deps"	
+	# TODO: implement MinGW	
 fi
-# TODO: implement MinGW
+
 
