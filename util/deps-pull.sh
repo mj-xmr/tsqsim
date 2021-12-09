ed1c43ecdc07df6eb844edbe40fedd50804c42b7
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2021-12-09 22:33:54 +0100

    Win unify scripts 01 (#30)

--- util/deps-pull.sh
+++ util/deps-pull.sh
@@ -8,12 +8,13 @@ if [ "$(uname)" == "Darwin" ]; then
 	fi
 	#HOMEBREW_NO_AUTO_UPDATE=1 brew install qt5
 	#brew link -v qt5 --force
-	HOMEBREW_NO_AUTO_UPDATE=1 brew install boost openssl ccache gnuplot eigen gsl unittest-cpp
+	HOMEBREW_NO_AUTO_UPDATE=1 brew install boost ccache gnuplot eigen gsl unittest-cpp # openssl zmq
 	# TODO: install qt instead of qt5, after moving to latest QCustomPlot. Then the "--prefix" and "link" aren't needed.
-elif [ "$(uname)" == "MinGw" ]; then
-	echo "TODO: install deps"	
+elif [ "$(uname)" == "Linux" ]; then
+	sudo apt update; sudo apt -y install build-essential cmake ccache gnuplot libeigen3-dev libunittest++-dev libgsl-dev libboost-system-dev libboost-filesystem-dev libboost-iostreams-dev libboost-serialization-dev libboost-program-options-dev libboost-date-time-dev libboost-atomic-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools #  libssl-dev
 else
-	sudo apt update; sudo apt -y install build-essential cmake ccache gnuplot libssl-dev libeigen3-dev libunittest++-dev libgsl-dev libboost-system-dev libboost-filesystem-dev libboost-iostreams-dev libboost-serialization-dev libboost-program-options-dev libboost-date-time-dev libboost-atomic-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
+	echo "TODO: install deps"	
+	# TODO: implement MinGW	
 fi
-# TODO: implement MinGW
+
 
