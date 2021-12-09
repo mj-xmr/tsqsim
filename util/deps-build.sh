164900f293daf7da3477ba57d55b2e3ded749d45
mj-xmr <mjxmr@protonmail.com>
2021-12-09 11:37:45 +0100

    Prep Win scripts

--- util/deps-build.sh
+++ util/deps-build.sh
@@ -2,10 +2,9 @@
 
 echo "Building dependencies"
 # externals/URT/build/build-urt.sh # Not needed anymore. Using CMakeLists.txt
-if [ "$(uname)" != "Darwin" ]; then
+if [ "$(uname)" != "Darwin" && "$(uname)" != "MinGw" ]; then
 	externals/scripts/build-qcustomplot.sh
 fi
-
 
 # Other dependencies here...
 
