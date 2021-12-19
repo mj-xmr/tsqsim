78f82e49c7f5af949cf61a556d2567e47ae6c725
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2021-12-19 10:41:51 +0100

    Moved scripts under static/scripts (#42)

--- cmake/util-tsqsim.cmake
+++ cmake/util-tsqsim.cmake
@@ -6,9 +6,9 @@ function (tsqSetupSymlinks target)
 	# TODO: use a loop for [data, cfg, scripts]
 	enjoSymlink(${target} ../../data 	${CMAKE_BINARY_DIR}/bin/data)	# Common data dir
 	enjoSymlink(${target} ../../cfg  	${CMAKE_BINARY_DIR}/bin/cfg)	# Common cfg  dir
-	enjoSymlink(${target} ../../scripts ${CMAKE_BINARY_DIR}/bin/scripts)	# Common data dir
-	enjoSymlink(${target} ../../../data ${CMAKE_CURRENT_BINARY_DIR}/data)	# Common data dir
-	enjoSymlink(${target} ../../../cfg  ${CMAKE_CURRENT_BINARY_DIR}/cfg)	# Common cfg  dir
-	enjoSymlink(${target} ../../../scripts ${CMAKE_CURRENT_BINARY_DIR}/scripts)	# Common data dir
+	enjoSymlink(${target} ../../static    	${CMAKE_BINARY_DIR}/bin/static)	# Common static dir
+	enjoSymlink(${target} ../../../data   ${CMAKE_CURRENT_BINARY_DIR}/data)	# Common data dir
+	enjoSymlink(${target} ../../../cfg    ${CMAKE_CURRENT_BINARY_DIR}/cfg)	# Common cfg  dir
+	enjoSymlink(${target} ../../../static ${CMAKE_CURRENT_BINARY_DIR}/static)	# Common static dir
 endfunction()
 
