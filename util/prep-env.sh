30643d7a619f211236efd0e58d208814520bb3d6
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-05-22 15:47:32 +0200

    Downloading TXTS & TSDIFF

--- util/prep-env.sh
+++ util/prep-env.sh
@@ -1,15 +1,26 @@
 #!/bin/bash -e
 
-DATA_FILE_BASE=TXTS.csv
 URL="http://cryptog.hopto.org/monero/data/"
-if [[ "$(uname)" == "Linux"  || "$(uname)" == "Darwin" ]]; then
-	DATA_FILE=$DATA_FILE_BASE.gz
-else
-	#DATA_FILE=$DATA_FILE_BASE.zip
-	DATA_FILE=$DATA_FILE_BASE
-fi
 
-DATA_URL=$URL/$DATA_FILE
+dload () {
+	DATA_FILE_BASE=$1
+	if [[ "$(uname)" == "Linux"  || "$(uname)" == "Darwin" ]]; then
+		DATA_FILE=$DATA_FILE_BASE.gz
+	else
+		#DATA_FILE=$DATA_FILE_BASE.zip
+		DATA_FILE=$DATA_FILE_BASE
+	fi
+	
+	DATA_URL=$URL/$DATA_FILE
+	
+	if [ -f "$DATA_FILE" ]; then
+		echo "Already exists: '`pwd`/$DATA_FILE'"
+	else
+		wget "$DATA_URL"
+	fi
+}
+
+
 DATA_DIR=build/data/data/txt
 CFG_DIR=build/cfg
 SCRIPTS_DIR=build/scripts
@@ -20,11 +31,8 @@ mkdir -pv $DATA_DIR
 pushd $DATA_DIR
 	mkdir -p xmr
 	pushd xmr
-		if [ -f "$DATA_FILE" ]; then
-			echo "Already exists: '`pwd`/$DATA_FILE'"
-		else
-			wget "$DATA_URL"
-		fi
+		dload "TXTS.csv"
+		dload "TSDIFF.csv"
 	popd
 popd
 
