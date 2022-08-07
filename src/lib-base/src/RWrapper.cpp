3313e24f6e547c7ad501cb6ea0e36c75d0d52f41
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-08-07 13:47:40 +0200

    Update RWrapper.cpp

--- src/lib-base/src/RWrapper.cpp
+++ src/lib-base/src/RWrapper.cpp
@@ -21,18 +21,19 @@ RWrapper::~RWrapper()
 /**
  * Wrapper for R function predict, defined in func.R.
  */
-EnjoLib::VecD RWrapper::R_predictVec(const EnjoLib::VecD & vec)
+EnjoLib::VecD RWrapper::R_predictVec(const EnjoLib::VecD & vecd)
 {
+    const std::vector<double> vec = vecd.ToVecDouble();
     EnjoLib::VecD retVec;   
 #ifdef USE_R
-    const EnjoLib::FP * a = vec.data();
+    const double * a = vec.data();
     const int alen = vec.size();
     const char * functionName = "predict";
 
     // Allocate an R vector and copy the C array into it.
     SEXP arg;
     PROTECT(arg = allocVector(REALSXP, alen));
-    std::memcpy(REAL(arg), a, alen * sizeof(EnjoLib::FP));
+    std::memcpy(REAL(arg), a, alen * sizeof(double));
 
     // Setup a call to the R function
     SEXP add1_call;
@@ -45,7 +46,7 @@ EnjoLib::VecD RWrapper::R_predictVec(const EnjoLib::VecD & vec)
     if (!errorOccurred)
     {
         printf("R returned: ");
-        EnjoLib::FP *val = REAL(ret);
+        double *val = REAL(ret);
         for (int i = 0; i < LENGTH(ret); i++)
         {
             printf("%0.1f, ", val[i]);
