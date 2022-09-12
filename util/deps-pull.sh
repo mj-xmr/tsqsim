718e61e19cf7116be6dcf4ed52357601b4bdf061
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-09-12 12:12:30 +0200

    qt5

--- util/deps-pull.sh
+++ util/deps-pull.sh
@@ -6,8 +6,8 @@ if [ "$(uname)" == "Darwin" ]; then
 	else 
 		xcode-select --install
 	fi
-	HOMEBREW_NO_AUTO_UPDATE=1 brew install qt llvm
-	#brew link -v qt --force
+	HOMEBREW_NO_AUTO_UPDATE=1 brew install qt5 llvm
+	brew link -v qt5 --force
 	#HOMEBREW_NO_AUTO_UPDATE=1 brew install boost ccache gnuplot eigen gsl unittest-cpp wxwidgets r qt # openssl zmq
 	# TODO: install qt instead of qt5, after moving to latest QCustomPlot. Then the "--prefix" and "link" aren't needed.
 	# Expose R_HOME path:
