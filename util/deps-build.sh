545a04530586180fb390e7ba0fe25e5dfd633313
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-09-12 10:18:26 +0200

    OS fix

--- util/deps-build.sh
+++ util/deps-build.sh
@@ -2,26 +2,28 @@
 
 echo "Building dependencies"
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
+if [[ "$OSTYPE" == linux* ]]; then
+	echo "$OSTYPE"
 	externals/scripts/build-qcustomplot.sh
 	externals/scripts/build-qcustomplot-2.sh
 #fi
 #elif [ "$(uname)" == "MinGw" ]; then
 #	echo "TODO: install deps"	
 #else
-elif [[ "$OSTYPE" == "dawrin" ]]; then
+elif [[ "$OSTYPE" == dawrin* ]]; then
+	echo "$OSTYPE"
 	externals/scripts/build-qcustomplot-2.sh
 # Other dependencies here...
-elif [[ "$OSTYPE" == "cygwin" ]]; then
+elif [[ "$OSTYPE" == cygwin* ]]; then
 	echo "$OSTYPE"
         # POSIX compatibility layer and Linux environment emulation for Windows
-elif [[ "$OSTYPE" == "msys" ]]; then
+elif [[ "$OSTYPE" == msys* ]]; then
 	echo "$OSTYPE"
         # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-elif [[ "$OSTYPE" == "win32" ]]; then
+elif [[ "$OSTYPE" == win32* ]]; then
 	echo "$OSTYPE"        
 	# I'm not sure this can happen.
-elif [[ "$OSTYPE" == "freebsd"* ]]; then
+elif [[ "$OSTYPE" == freebsd* ]]; then
   echo "$OSTYPE"      # ...
 else
 	echo "Unknown OS: $OSTYPE"
