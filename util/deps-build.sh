882e4917871bd9569bda4b47c0cd4d09f4df0b97
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-09-12 09:24:54 +0200

    Infra: Prep. for QCustomPlot 2

--- util/deps-build.sh
+++ util/deps-build.sh
@@ -4,6 +4,7 @@ echo "Building dependencies"
 # externals/URT/build/build-urt.sh # Not needed anymore. Using CMakeLists.txt
 if [ "$(uname)" == "Linux" ]; then
 	externals/scripts/build-qcustomplot.sh
+	externals/scripts/build-qcustomplot-2.sh
 fi
 #elif [ "$(uname)" == "MinGw" ]; then
 #	echo "TODO: install deps"	
