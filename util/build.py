2b21a7339940c2ad4a1ef3ba54711f671fdc2f9b
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-09-12 17:19:18 +0200

    Final

--- util/build.py
+++ util/build.py
@@ -27,7 +27,7 @@ OFF='OFF'
 ON='ON'
 #NL=' \ \n'
 NL=' '
-MAC_QT_LIBPATH=' export PATH="/usr/local/opt/qt@5/bin:$PATH;"'
+MAC_QT_LIBPATH=' export PATH="/usr/local/opt/qt@5/bin:$PATH"; '
 
 def get_parser():
     parser = argparse.ArgumentParser()
