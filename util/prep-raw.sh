c891f682bce646a9c6cb14888b6ee08b035f1007
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-10-24 16:13:43 +0200

    make monero

--- util/prep-raw.sh
+++ util/prep-raw.sh
@@ -3,6 +3,10 @@
 # Uses the Monero blockchain itself to extract the raw data.
 # Warning! It needs a lot of disk space to do this. Expect even ~100 GB.
 
+pushd $HOME/monero 
+	make -j2
+popd
+
 cd externals/transactions-export-mj
 mkdir -p build && cd build
 pwd
