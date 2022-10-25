003f3cedfb2c9a3c7fd0ef1fdac2c0e5230de867
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-10-25 11:32:33 +0200

    Final

--- util/prep-raw.sh
+++ util/prep-raw.sh
@@ -3,6 +3,10 @@
 # Uses the Monero blockchain itself to extract the raw data.
 # Warning! It needs a lot of disk space to do this. Expect even ~100 GB.
 
+echo "Installing 'monero' dependencies..."
+
+NPROC=2
+
 sudo apt update
 sudo apt install build-essential cmake pkg-config libssl-dev libzmq3-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libexpat1-dev libpgm-dev libhidapi-dev libusb-1.0-0-dev libprotobuf-dev protobuf-compiler libudev-dev libboost-chrono-dev libboost-date-time-dev libboost-filesystem-dev libboost-locale-dev libboost-program-options-dev libboost-regex-dev libboost-serialization-dev libboost-system-dev libboost-thread-dev python3 ccache
 
@@ -13,10 +17,11 @@ pushd $HOME
 	pushd monero
 		git checkout master
 		git submodule init && git submodule update --remote; git submodule sync && git submodule update
-		make -j2
+		make -j$NPROC
 	popd
 popd
 
+echo "Installing 'transactions-export' dependencies..."
 sudo apt install libcurl4-gnutls-dev
 cd externals/transactions-export-mj
 mkdir -p build && cd build
@@ -25,5 +30,7 @@ if ! cmake .. ; then
 	echo "Is your 'monero' directory symlinked to $HOME and master built with 'make'?"
 	exit 1
 fi
+make -j$NPROC
 
-make
+echo "Now synchronize the (full) blockchain and use the 'xmr2csv' executable."
+
