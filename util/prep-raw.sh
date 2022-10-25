afebe550a4d871e48275da72ee7a2dfdc61ab7a9
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-10-25 08:19:56 +0200

    Clone monero

--- util/prep-raw.sh
+++ util/prep-raw.sh
@@ -3,8 +3,16 @@
 # Uses the Monero blockchain itself to extract the raw data.
 # Warning! It needs a lot of disk space to do this. Expect even ~100 GB.
 
-pushd $HOME/monero 
-	make -j2
+
+pushd $HOME
+	if [ ! -d monero ]; then
+		git clone --recursive https://github.com/monero-project/monero.git
+	fi
+	pushd monero
+		git submodule init && git submodule update --remote; git submodule sync && git submodule update
+		git checkout master
+		make -j2
+	popd
 popd
 
 cd externals/transactions-export-mj
